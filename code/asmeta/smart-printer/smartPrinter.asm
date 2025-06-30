b06cce8c0c298ea61440c3205522a04e7d6fad16
Davide Gamba <gamba.davide1996@gmail.com>
2025-06-30 19:18:21 +0200

    Setting delle classi Java e dell'asm

---
+++ code/asmeta/smart-printer/smartPrinter.asm
@@ -0,0 +1,12 @@
+asm smartPrinter
+
+import StandardLibrary
+
+signature:
+
+
+definitions:
+
+
+default init s0:
+
---
+++ code/java/smart_printer/test/printer/SmartPrinterTest.java
@@ -0,0 +1,14 @@
+package printer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SmartPrinterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
