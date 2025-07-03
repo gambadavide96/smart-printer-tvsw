3f9c6697fcd12339f258b0436b6a25a88e38e051
Davide Gamba <gamba.davide1996@gmail.com>
2025-07-03 12:11:11 +0200

    Update smartPrinter.asm

--- code/asmeta/smart-printer/smartPrinter.asm
+++ code/asmeta/smart-printer/smartPrinter.asm
@@ -8,6 +8,8 @@ signature:
 	abstract domain Badge
 	enum domain Stato = {SPENTA | AVVIO | MOSTRABADGE | INSERISCIPIN | PRONTA | INUSO | ERRORE | OUTOFSERVICE}
 	enum domain Servizio = {PRINTBN | PRINTCOL | SCANSIONE | NONE}
+	enum domain Accensione = {ON | OFF}
+	enum domain StatoMacchina = {GUASTA | NONGUASTA}
 	domain QuantitaCarta subsetof Integer	// Il numero di fogli per la stampa
 	domain LivelloToner subsetof Integer	// Il livello del toner in %
 	domain Secondi subsetof Integer
@@ -19,9 +21,10 @@ signature:
 	dynamic controlled currentBadge: Badge
 	dynamic controlled userCredit: Badge -> Integer
 	dynamic controlled secondi: Secondi
+	dynamic controlled message: String
 	
-	dynamic monitored onOff: Boolean
-	dynamic monitored spiaGuasto: Boolean
+	dynamic monitored onOff: Accensione
+	dynamic monitored spiaGuasto: StatoMacchina
 	dynamic monitored insertedBadge: Badge 
 	dynamic monitored insertedPin: Integer 
 	dynamic monitored selectedService: Servizio
@@ -30,11 +33,8 @@ signature:
 	
 	derived checkCredit: Badge -> Boolean
 	
-	static badge1: Badge
-	static badge2: Badge
-	static none: Badge
-	
-	
+	static davide: Badge
+	static matteo: Badge
 	
 definitions:
 
@@ -44,25 +44,29 @@ definitions:
 	
 	function pin($b in Badge) =
 		switch($b)
-			case badge1 : 1
-			case badge2 : 2
+			case davide : 1
+			case matteo : 2
 		endswitch
 	
 	//Controlla che l'utente abbia abbastanza credito per l'operazione di stampa
 	function checkCredit($b in Badge) =
 		userCredit($b) >= 50
 	
-	rule r_accendiStampante($b in Boolean) = 
-		if(printerState = SPENTA and $b = true) then
-			printerState := AVVIO
+	macro rule r_accendiStampante($b in Accensione) = 
+		if(printerState = SPENTA and $b = ON) then
+				par
+					printerState := AVVIO
+					message := "Accensione in corso"
+				endpar
 		endif
 	
 	//la macchina si avvia in 3s, se sorge un problema si accende spia guasto e passa in outofservice
-	rule r_avvioStampante($g in Boolean) = 
-		if(spiaGuasto) then
+	rule r_avvioStampante = 
+		if(spiaGuasto = GUASTA) then
 			par
 				printerState := OUTOFSERVICE
 				secondi := 0 
+				message := "Stampante guasta"
 			endpar
 		else
 			if(secondi < 1) then
@@ -70,33 +74,52 @@ definitions:
 			else
 				par
 					printerState := MOSTRABADGE
-					secondi := 0 
+					secondi := 0
+					message := "Mostrare Badge"
 				endpar
 			endif
 		endif
 		
 	//Se il guasto è stato riparato, la macchina si avvia, altrimenti rimane outofservice	
-	rule r_gestioneGuasto($g in Boolean) = 
-		if(not $g) then
-			printerState := AVVIO
+	rule r_gestioneGuasto($g in StatoMacchina) = 
+		if($g = NONGUASTA) then
+			par
+				printerState := AVVIO
+				message := "Accensione in corso"
+			endpar
 		endif
 		
 	rule r_identificazione_utente =
 		if(exist $b in Badge with $b = insertedBadge) then
 				par
 					currentBadge := insertedBadge
-					printerState := PRONTA
+					printerState := INSERISCIPIN
+					message := "Inserire Pin"
 				endpar
 		endif
+		
+	rule r_inserimento_pin = 
+		if(insertedPin = pin(currentBadge)) then
+				par
+					printerState := PRONTA
+					message := "Selezionare una azione"
+				endpar
+		else
+			par
+				printerState := MOSTRABADGE
+				message := "Pin errato"
+				endpar
+		endif
+		
 	
 	//Regola per gestire l'operativa della stampante una volta che è pronta
 	//Posso spegnerla oppure lanciare un lavoro
-	macro rule r_utilizzoStampante($b in Boolean,$s in Servizio) =
-		if (printerState = PRONTA and $b = true) then
-			par
-				printerState := SPENTA
-				currentBadge := none	//modificare questa parte
-			endpar
+	macro rule r_utilizzoStampante =
+		if (printerState = PRONTA and onOff = OFF) then
+				par
+					printerState := SPENTA
+					message := "Premere On per accendere"
+				endpar
 		endif
 		
 	main rule r_main = 
@@ -104,13 +127,15 @@ definitions:
 			case SPENTA:
 				r_accendiStampante[onOff]
 			case AVVIO:
-				r_avvioStampante[spiaGuasto]
+				r_avvioStampante[]
 			case OUTOFSERVICE:
 				r_gestioneGuasto[spiaGuasto]
 			case MOSTRABADGE:
 				r_identificazione_utente[]
+			case INSERISCIPIN:
+				r_inserimento_pin[]
 			case PRONTA:
-				r_utilizzoStampante[onOff,selectedService]
+				r_utilizzoStampante[]
 		endswitch
 		
 	
@@ -125,7 +150,10 @@ default init s0:
 	function fogliCarta = 200
 	
 	function userCredit($b in Badge) = switch($b)
-										case badge1 : 1000
-										case badge2 : 1000
+										case davide : 1000
+										case matteo : 1000
 									endswitch
+	
+	function message = "Premere ON per accendere"
+	
 
